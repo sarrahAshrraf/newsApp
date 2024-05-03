a3413386e6904bda0c8a315819d78e07a5e7404d
sarrahAshrraf <154979941+sarrahAshrraf@users.noreply.github.com>
2024-05-02 18:18:59 -0700

    inital commit

---
+++ NEWSApp/NewsCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCell.swift
+//  NEWSApp
+//
+//  Created by sarrah ashraf on 29/04/2024.
+//
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+    
+    @IBOutlet weak var newAuthor: UILabel!
+    @IBOutlet weak var newImg: UIImageView!
+}
